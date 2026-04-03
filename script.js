73c3ae2f895f2d5babdf4ccc1937188cf49dd006
Just Mediocre <68330655+normalycreate@users.noreply.github.com>
2026-04-03 15:59:00 +0700

    Adding a navbar - WIP

---
+++ script.js
@@ -0,0 +1,17 @@
+// Just easter egg in inspect menu
+const asciiArt = `
+  _____         _      ___ _               
+ |_   _|_ _ ___| | __ | __| |_____      __
+   | |/ _\` / __| |/ / | _|| |/ _ \\ \\ /\\ / /
+   | | (_| \\__ \\   <  | | | | (_) \\ V  V / 
+   |_|\\__,_|___/_|\\_\\ |_| |_|\\___/ \\_/\\_/  
+`;
+
+console.log(
+  `%c${asciiArt}`,
+  "color: #00FF00; font-weight: bold; font-family: monospace;"
+);
+console.log(
+  "%c> Succefully Running Javascript (Taskflow WIP-RELEASE--BATCH-01)\n> Welcome To Task Flow! Ready to written your schedule today? ☕",
+  "color: #00FF00; font-family: monospace; font-size: 12px;"
+);
